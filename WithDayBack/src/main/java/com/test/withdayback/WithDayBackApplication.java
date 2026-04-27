c012271004cd8a09a90d6c6d93ba2af77794827e
JavaSinged <shinjiwoong1656@gmail.com>
2026-04-27 10:24:39 +0900

    fistCommit

---
+++ WithDayBack/src/main/java/com/test/withdayback/WithDayBackApplication.java
@@ -0,0 +1,13 @@
+package com.test.withdayback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithDayBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WithDayBackApplication.class, args);
+    }
+
+}
---
+++ WithDayBack/src/test/java/com/test/withdayback/WithDayBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.withdayback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithDayBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
