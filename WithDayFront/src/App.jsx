90442d1eef9fb853b512d96573b2f7377b75584c
Choi, Jiwoo <gamjachoi00@naver.com>
2026-05-03 17:18:44 +0900

    캘린더 추가

--- WithDayFront/src/App.jsx
+++ WithDayFront/src/App.jsx
@@ -3,7 +3,8 @@ import Home from "./page/home/Home";
 import BottomNav from "./widgets/BottomNav/BottomeNav";
 import Header from "./widgets/Header/Header";
 import { Routes, Route } from "react-router-dom";
-import SignupForm from './page/login/SignupForm';
+import SignupForm from "./page/login/SignupForm";
+import WriteSchedule from "./page/schedule/WriteSchedule";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/join" element={<SignupForm />} />
+        <Route path="/write" element={<WriteSchedule />} />
       </Routes>
       <BottomNav />
     </div>
--- WithDayFront/src/page/schedule/WriteSchedule.jsx
+++ WithDayFront/src/page/schedule/WriteSchedule.jsx
@@ -1,17 +1,30 @@
-import { Input } from "../../widgets/commons/Form";
+import { Input, TextArea } from "../../shared/ui/Form/Form";
 import styles from "./WriteSchedule.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+import { DateRange } from "react-date-range";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import { ko } from "date-fns/locale";
+
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { registerLocale } from "react-datepicker";
+
+registerLocale("ko", ko);
 
 const WriteSchedule = () => {
   const [post, setPost] = useState({
     title: "",
-    contentHTML: "",
-    contentText: "",
+    description: "",
     category: "",
     region: "",
-    startDate: "",
-    endDate: "",
-    recruitmentPeriod: "",
+    detailRegion: "",
+    startDate: new Date(),
+    endDate: new Date(),
+    recruitmentPeriod: new Date(),
     maxParticipants: "",
     minAge: "",
     maxAge: "",
@@ -21,65 +34,238 @@ const WriteSchedule = () => {
     thumbnail: "",
   });
 
+  const categories = ["전체", "여행", "팝업", "식사", "액티비티"];
+
+  const categoryOptions = categories.map((item, index) => ({
+    value: index,
+    label: item,
+  }));
+
+  /* 시 */
+  const regions = [];
+
+  const regionOptions = regions.map((item, index) => ({
+    value: index,
+    label: item,
+  }));
+
+  useEffect(() => {
+    axios.get().then(regions).catch();
+
+    const regionOptions = regions.map((item, index) => ({
+      value: index,
+      label: item,
+    }));
+  }, []);
+
+  /* 군, 구 */
+
+  const detailRegions = [];
+
+  const detailRegionOptions = regions.map((item, index) => ({
+    value: index,
+    label: item,
+  }));
+
+  /* 시 선택되면 세부 지역 불러옴 */
+  useEffect(() => {
+    axios.get().then(detailRegions).catch();
+
+    const detailRegionOptions = detailRegions.map((item, index) => ({
+      value: index,
+      label: item,
+    }));
+  }, [post.region]);
+
   return (
-    <main className={styles.main}>
-      <div></div>
-      <div>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-          }}
-          autoComplete="off"
-        >
-          <ul className={`${styles.info_input_wrap} ${styles.member_name}`}>
-            <li>
-              <label htmlFor="title">Title</label>
-            </li>
-            <li>
-              <Input
-                type="text"
-                name="title"
-                id="title"
-                onChange={(e) => {
-                  setPost({ ...post, title: e.target.value });
-                }}
-              ></Input>
-            </li>
-          </ul>
-        </form>
-      </div>
-    </main>
+    <>
+      <header className={styles.header}>
+        <BackButton />
+      </header>
+      <main className={styles.main}>
+        <div className={styles.content_wrap}>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+            autoComplete="off"
+          >
+            <div className={styles.input_content_wrap}>
+              <h2 className={styles.input_title}>기본 정보</h2>
+              <ul className={`${styles.input_wrap} ${styles.title}`}>
+                <li>
+                  <label htmlFor="title">Title</label>
+                </li>
+                <li>
+                  <Input
+                    type="text"
+                    name="title"
+                    id="title"
+                    placeholder="Title"
+                    value={post.title}
+                    onChange={(e) => {
+                      setPost({ ...post, title: e.target.value });
+                    }}
+                  ></Input>
+                </li>
+              </ul>
+              <ul className={`${styles.input_wrap} ${styles.description}`}>
+                <li>
+                  <label htmlFor="description">Description</label>
+                </li>
+                <li>
+                  <TextArea
+                    name="description"
+                    id="description"
+                    placeholder="description"
+                    value={post.description}
+                    onChange={(e) => {
+                      setPost({ ...post, description: e.target.value });
+                    }}
+                  ></TextArea>
+                </li>
+              </ul>
+              <ul className={`${styles.input_wrap} ${styles.category}`}>
+                <li>
+                  <label htmlFor="category">Category</label>
+                </li>
+                <li>
+                  <select>
+                    {categoryOptions.map((item) => (
+                      <option key={item.value} value={item.value}>
+                        {item.label}
+                      </option>
+                    ))}
+                  </select>
+                </li>
+              </ul>
+              <ul className={`${styles.input_wrap} ${styles.region}`}>
+                <li>
+                  <label htmlFor="region">Region</label>
+                </li>
+                <li>
+                  <select>
+                    <option value="">시/도</option>
+                    {regionOptions.map((item) => (
+                      <option key={item.value} value={item.value}>
+                        {item.label}
+                      </option>
+                    ))}
+                  </select>
+                </li>
+              </ul>
+              <ul className={`${styles.input_wrap} ${styles.detailRegion}`}>
+                <li>
+                  <label htmlFor="detailRegion">DetailRegion</label>
+                </li>
+                <li>
+                  <select>
+                    <option value="">시/군/구</option>
+                    {detailRegionOptions.map((item) => (
+                      <option key={item.value} value={item.value}>
+                        {item.label}
+                      </option>
+                    ))}
+                  </select>
+                </li>
+              </ul>
+            </div>
+            <div className={styles.input_content_wrap}>
+              <h2 className={styles.input_title}>일정 정보</h2>
+              <CalendarRange
+                post={post}
+                setPost={setPost}
+                months={1}
+                direction="horizontal"
+              />
+              <ul
+                className={`${styles.input_wrap} ${styles.recruitmentPeriod}`}
+              >
+                <li>
+                  <label>모집 마감일</label>
+                </li>
+                <li>
+                  <DatePicker
+                    locale="ko"
+                    selected={post.recruitmentPeriod}
+                    onChange={(date) =>
+                      setPost((prev) => ({
+                        ...prev,
+                        recruitmentPeriod: date,
+                      }))
+                    }
+                    // 마감일은 일정 시작일 하루 전 날까지
+                    maxDate={post.startDate - 1}
+                    dateFormat="yyyy년 MM월 dd일"
+                    placeholderText="날짜 선택"
+                  />
+                </li>
+              </ul>
+            </div>
+          </form>
+        </div>
+      </main>
+    </>
   );
 };
 
-/**
- * 컴포넌트 사용 예시
-import Button from "@/shared/ui/Button/Button";
-import Input from "@/shared/ui/Input/Input";
+const BackButton = () => {
+  const navigate = useNavigate();
 
-function Example() {
   return (
-    <>
-      <Input label="이메일" placeholder="email 입력" />
-
-      <Button variant="primary">로그인</Button>
-      <Button variant="accent">회원가입</Button>
-      <Button variant="outline">취소</Button>
-    </>
+    <button className={styles.backButton} onClick={() => navigate(-1)}>
+      {"<"}
+    </button>
   );
-}
+};
 
-import { Input, TextArea } from "@/shared/ui/Form/Form";
+const CalendarRange = ({ post, setPost }) => {
+  const [state, setState] = useState([
+    {
+      startDate: post.startDate || new Date(),
+      endDate: post.endDate || new Date(),
+      key: "selection",
+    },
+  ]);
 
-function Example() {
+  const handleChange = (item) => {
+    const selection = item.selection;
+
+    setState([selection]);
+
+    setPost((prev) => ({
+      ...prev,
+      startDate: selection.startDate,
+      endDate: selection.endDate,
+    }));
+  };
+
   return (
-    <>
-      <Input placeholder="이름 입력" />
-      <Input placeholder="이메일" error />
+    <div className={styles.calendarWrapper}>
+      <DateRange
+        locale={ko}
+        ranges={state}
+        onChange={handleChange}
+        moveRangeOnFirstSelection={false}
+        editableDateInputs={true}
+        showMonthAndYearPickers={true}
+        months={1}
+        direction="horizontal"
+      />
 
-      <TextArea placeholder="내용 입력" />
-    </>
+      <div className={styles.dateWrap}>
+        <ul className={`${styles.input_wrap} ${styles.duringDate}`}>
+          <li>
+            <label>일정</label>
+          </li>
+          <li>
+            <p>시작일: {post.startDate?.toLocaleDateString()}</p>
+            <p>종료일: {post.endDate?.toLocaleDateString()}</p>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
-}
- * 
- */
+};
+
+export default WriteSchedule;
